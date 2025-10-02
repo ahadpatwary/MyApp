5579d984c1f80e22b290cc08164f6cbb5b6ac0e0
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2025-10-03 00:26:13 +0600

    scrool bar customized

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,7 +1,7 @@
 "use client"
-
+import { CiCircleChevRight } from "react-icons/ci";
 import * as React from "react"
-import { GripVerticalIcon } from "lucide-react"
+// import { GripVerticalIcon } from "lucide-react"
 import * as ResizablePrimitive from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
@@ -40,13 +40,14 @@ function ResizableHandle({
       data-slot="resizable-handle"
       className={cn(
         "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90",
+        " w-5 bg-transparent border-l-1",
         className
       )}
       {...props}
     >
       {withHandle && (
-        <div className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs border">
-          <GripVerticalIcon className="size-2.5" />
+        <div className=" z-10 items-center justify-centerborder">
+          <CiCircleChevRight size={30} />
         </div>
       )}
     </ResizablePrimitive.PanelResizeHandle>
