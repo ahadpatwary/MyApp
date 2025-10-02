d1ecf208f9a9b4c41334888dcc524326c940d6e3
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2025-10-02 22:00:55 +0600

    sideBar optimize

--- src/components/ResizableDemo.tsx
+++ src/components/ResizableDemo.tsx
@@ -1,50 +1,55 @@
 'use client';
+
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/components/ui/resizable"
-import { ShowCard } from '@/components/ShowCard'
-import { MenubarDemo } from '@/components/Bar'
-import { UserProfile } from '@/components/UserProfile'
-import { CustomWrapper } from '@/components/CustomWrapper'
+} from "@/components/ui/resizable";
+import { ShowCard } from '@/components/ShowCard';
+import { MenubarDemo } from '@/components/Bar';
+import { UserProfile } from '@/components/UserProfile';
+import { CustomWrapper } from '@/components/CustomWrapper';
 
-
-interface dotProps{
-  Dot : boolean;
+interface DotProps {
+  Dot: boolean;
 }
 
-export function ResizableDemo({ Dot }: dotProps) {
+export function ResizableDemo({ Dot }: DotProps) {
   return (
     <>
       <MenubarDemo />
-      <div className = "h-15 w-screen"> </div>
-    
-      <ResizablePanelGroup direction="horizontal" className="w-full border">
-        <ResizablePanel defaultSize={30} minSize={20} maxSize={40}>
-          <UserProfile dot ={Dot} />
+      <div className="h-15 w-screen"></div>
+
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="w-full flex flex-col md:flex-row border"
+      >
+        {/* Sidebar */}
+        <ResizablePanel
+          defaultSize={30}
+          minSize={20}
+          maxSize={40}
+          className="order-1 md:order-none w-full md:w-auto"
+        >
+          <UserProfile dot={Dot} />
         </ResizablePanel>
 
-        <ResizableHandle />
+        {/* Resizable handle */}
+        <ResizableHandle className="hidden md:block" />
 
-        <ResizablePanel defaultSize={70}>
+        {/* Main Content */}
+        <ResizablePanel defaultSize={70} className="order-2 md:order-none w-full">
           <CustomWrapper>
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
-            <ShowCard dot ={Dot} />
+            <ShowCard dot={Dot} />
+            <ShowCard dot={Dot} />
+            <ShowCard dot={Dot} />
+            <ShowCard dot={Dot} />
+            <ShowCard dot={Dot} />
+            <ShowCard dot={Dot} />
+            <ShowCard dot={Dot} />
           </CustomWrapper>
         </ResizablePanel>
       </ResizablePanelGroup>
     </>
-  )
+  );
 }
