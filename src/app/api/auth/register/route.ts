99fbb812de12a614ba428080024533a42a47e85f
Abdul Ahad Patwary <abdulahadpatwary2006@gmail.com>
2025-10-04 08:33:08 +0600

    export change

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -38,6 +38,7 @@ export async function POST(request : NextRequest){
         )
         
     }catch(err){
+        console.log(err);
         return NextResponse.json(
             { error: "Failed to register user"},
             { status: 502}
