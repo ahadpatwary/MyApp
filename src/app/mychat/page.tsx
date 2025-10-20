b341db23612645055a3df8509a654342dc75b59f
Abdul Ahad Patwary <168383212+ahadpatwary@users.noreply.github.com>
2025-10-20 11:07:05 +0000

    change chat message

--- src/app/mychat/page.tsx
+++ src/app/mychat/page.tsx
@@ -1,8 +1,7 @@
 'use client'
 import ChatCard from "@/components/ChatCard"
 import { useSearchParams } from "next/navigation";
-
-
+import { Suspense } from "react";
 
 export default function Home(){
     const params = useSearchParams();
@@ -10,8 +9,10 @@ export default function Home(){
     const chatWith = params.get("chatWith");
 
     return (
-        <div className="h-screen w-screen flex justify-center items-center">
-            <ChatCard userId = {userId as string} chatWith = {chatWith as string}  />
-        </div>
+        <Suspense fallback={<div>Loading chat...</div>}>
+            <div className="h-screen w-screen flex justify-center items-center">
+                <ChatCard userId={userId as string} chatWith={chatWith as string} />
+            </div>
+        </Suspense>
     )
 }
